ec078a9fef62d50f6da5890a1c3b52e557201bbb
dev1 <red@1234567>
2024-09-28 22:46:09 +0600

    jpa flow

--- eosb_data_jpa/src/main/java/com/mhk/edj/EosbDataJpaApplication.java
+++ eosb_data_jpa/src/main/java/com/mhk/edj/EosbDataJpaApplication.java
@@ -11,3 +11,15 @@ public class EosbDataJpaApplication {
     }
 
 }
+/*
+JDBC --->   PgSQLDriver
+            MySQlDriver
+            OracleSqlDriver
+JPA-->  Hibernate
+        EclipsLink             ---> JDBC --> PgSqlDriver
+        Apache Open Jpa
+
+Spring Data Jpa -> Jpa ->  Hibernate
+                           EclipsLink             ---> JDBC    --> PgSqlDriver
+                           Apache Open Jpa
+        */
