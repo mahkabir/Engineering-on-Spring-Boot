efdf8e06b8120d33d5c7b7e403d5059e5936cfcf
dev1 <red@1234567>
2024-09-28 22:09:30 +0600

    jpa

---
+++ eosb_data_jpa/src/main/java/com/mhk/edj/EosbDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.mhk.edj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EosbDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EosbDataJpaApplication.class, args);
+    }
+
+}
