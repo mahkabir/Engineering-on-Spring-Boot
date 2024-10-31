300ca5a93195c3167432db7e57e0d0fe73b0e6df
Humayun <red@1234567>
2024-10-31 12:56:06 +0600

    company entity edited

--- eosb_validation_error_handling/src/main/java/com/mhk/eosb_validation_error_handling/company/management/domain/entity/Company.java
+++ eosb_validation_error_handling/src/main/java/com/mhk/eosb_validation_error_handling/company/management/domain/entity/Company.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -23,30 +24,51 @@ public class Company extends BaseEntity {
     private String address;
 
     @Column(name = "CONTACT_MOBILE")
-    private String contactPerson;
-
-    @Column(name = "CONTACT_MOBILE")
     private String contactMobile;
+
+    @Column(name = "BILLING_MOBILE")
+    private String billingNumber;
 
     @Column(name = "CONTACT_EMAIL")
     private String emailAddress;
 
-    @Column(name = "BILLING_NUMBER")
-    private String billingNumber;
+    @Column(name = "LBS_ADMIN_ID")
+    private Long lbsAdminId;
+
+    @Column(name = "LBS_ADMIN_NAME")
+    private String lbsAdminName;
+
+    @Column(name = "LBS_ADMIN_PASSWORD")
+    private  String lbsAdminPassword;
 
     @Column(name = "BILLING_AMOUNT")
     private BigDecimal billingAmount;
 
-    @Column(name = "DAILY_AMOUNT")
+    @Column(name = "DAILY_AMT")
     private BigDecimal dailyAmount;
+
+    @Column(name = "MONTHLY_AMT")
+    private BigDecimal monthlyAmount;
 
     @Column(name = "IS_ENABLE_CHARGING")
     private Boolean isEnableCharging;
 
-    @Column(name = "LOGO")
-    private String logo;
+    @Column(name = "LOGO_URL_LARGE")
+    private String logoUrlLarge;
+
+    @Column(name = "LOGO_URL_SMALL")
+    private String logoUrlSmall;
+
+    @Column(name = "TRACKER_TNAME")
+    private String trackerTName;
 
     @Column(name = "REMARKS")
     private String remarks;
+
+    @Column(name = "CREATED_AT")
+    private Date createdAt;
+
+    @Column(name = "UPDATED_AT")
+    private Date updatedAt;
 
 }
